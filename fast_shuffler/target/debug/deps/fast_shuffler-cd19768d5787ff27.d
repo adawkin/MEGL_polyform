1eb9badc1de1916ea66e0bd82e2f0a5459eb2190
Dhruv Gramopadhye <dhruv@null.black>
2022-05-02 13:46:07 -0400

    abandoned: list based omino

---
+++ fast_shuffler/target/debug/deps/fast_shuffler-cd19768d5787ff27.d
@@ -0,0 +1,5 @@
+/home/dgramop/projects/MEGL_polyform/fast_shuffler/target/debug/deps/fast_shuffler-cd19768d5787ff27: src/main.rs
+
+/home/dgramop/projects/MEGL_polyform/fast_shuffler/target/debug/deps/fast_shuffler-cd19768d5787ff27.d: src/main.rs
+
+src/main.rs:
---
+++ fast_shuffler/target/debug/fast_shuffler.d
@@ -0,0 +1,1 @@
+/home/dgramop/projects/MEGL_polyform/fast_shuffler/target/debug/fast_shuffler: /home/dgramop/projects/MEGL_polyform/fast_shuffler/src/main.rs
